f92ea8196b02c1faf8d41fc9efb6bfac194bfd73
GitHub Action <action@github.com>
2023-01-14 16:35:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/janjongerden/ideabranchpicker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/janjongerden/ideabranchpicker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.janjongerden.ideabranchpicker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.janjongerden.ideabranchpicker.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/janjongerden/ideabranchpicker/services/MyProjectService.kt
+++ src/main/kotlin/com/github/janjongerden/ideabranchpicker/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.janjongerden.ideabranchpicker.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.janjongerden.ideabranchpicker.MyBundle
 
 class MyProjectService(project: Project) {
 
